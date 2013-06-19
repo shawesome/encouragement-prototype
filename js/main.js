ed800725f47323f5903afdb473abc0b723bc277c
Martin Shaw <martin.shaw@meanbee.com>
2013-06-19 18:56:07 +0100

    Convert tabs to spaces

--- js/main.js
+++ js/main.js
@@ -1,97 +1,97 @@
-var jsApp	=
+var jsApp   =
 {
-	
-	/* ---
-	
-		Initialize the jsApp
-		
-		---			*/
-	onload: function() {
-		
-		if (!me.video.init('jsapp', 800, 480)) {
-			alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
-			return;
-		}
-	
-		// set all ressources to be loaded
-		me.loader.onload = this.loaded.bind(this);
-		
-		// set all ressources to be loaded
-		me.loader.preload(g_resources);
+    
+    /* ---
+    
+        Initialize the jsApp
+        
+        ---         */
+    onload: function() {
+        
+        if (!me.video.init('jsapp', 800, 480)) {
+            alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
+            return;
+        }
+    
+        // set all ressources to be loaded
+        me.loader.onload = this.loaded.bind(this);
+        
+        // set all ressources to be loaded
+        me.loader.preload(g_resources);
 
-		// load everything & display a loading screen
-		me.state.change(me.state.LOADING);
-	},
-	
-	
-	/* ---
-	
-		callback when everything is loaded
-		
-		---										*/
-	loaded: function ()
-	{
-		// set the "Play/Ingame" Screen Object
-		me.state.set(me.state.PLAY, this);
+        // load everything & display a loading screen
+        me.state.change(me.state.LOADING);
+    },
+    
+    
+    /* ---
+    
+        callback when everything is loaded
+        
+        ---                                     */
+    loaded: function ()
+    {
+        // set the "Play/Ingame" Screen Object
+        me.state.set(me.state.PLAY, this);
         
         me.entityPool.add("player", PlayerEntity);
 
-		// enable the keyboard (to navigate in the map)
-		me.input.bindKey(me.input.KEY.LEFT,	 "left");
-		me.input.bindKey(me.input.KEY.RIGHT, "right");
-		me.input.bindKey(me.input.KEY.UP,	 "up");
-		me.input.bindKey(me.input.KEY.DOWN,	 "down");
+        // enable the keyboard (to navigate in the map)
+        me.input.bindKey(me.input.KEY.LEFT,  "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP,    "up");
+        me.input.bindKey(me.input.KEY.DOWN,  "down");
 
         // overrides the entity gravity values
         me.sys.gravity = 0;
          
-		// start the game
-		me.state.change(me.state.PLAY);
-		
-	},
+        // start the game
+        me.state.change(me.state.PLAY);
+        
+    },
 
-	reset: function()
-	{	
-		me.game.reset();
-		// load a level
-		me.levelDirector.loadLevel("prototype");		
-	},
+    reset: function()
+    {   
+        me.game.reset();
+        // load a level
+        me.levelDirector.loadLevel("prototype");        
+    },
 
-	
-	/* ---
-	
-		 rendering loop
-		
-		---										*/
-	onUpdateFrame: function() {
-		// update the frame counter
-		me.timer.update();
+    
+    /* ---
+    
+         rendering loop
+        
+        ---                                     */
+    onUpdateFrame: function() {
+        // update the frame counter
+        me.timer.update();
 
-		// update our sprites
-		me.game.update();
-	
-		// draw the rest of the game
-		me.game.draw();
-	},
-	
-	/* ---
-	
-		 change the current level 
-		 using the listbox current value in the HTML file
-		
-		---										*/
-	changelevel: function() {
-		// reset everything
-		me.game.reset();
-		
-		// load the new level
-		me.levelDirector.loadLevel(level);		
-	}
+        // update our sprites
+        me.game.update();
+    
+        // draw the rest of the game
+        me.game.draw();
+    },
+    
+    /* ---
+    
+         change the current level 
+         using the listbox current value in the HTML file
+        
+        ---                                     */
+    changelevel: function() {
+        // reset everything
+        me.game.reset();
+        
+        // load the new level
+        me.levelDirector.loadLevel(level);      
+    }
 
 }; // jsApp
 
 
 //bootstrap :)
 window.onReady(function() {
-	jsApp.onload();
+    jsApp.onload();
 });
