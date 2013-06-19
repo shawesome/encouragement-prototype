120c9093a44421fa861e95852cafaea3bc825486
Martin Shaw <martin.shaw@meanbee.com>
2013-06-19 18:54:38 +0100

    Remove some stuff we don't need form main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/* -----
-
-	TileD loader 
-	
-	------					*/
-
 var jsApp	=
 {
 	
@@ -12,11 +6,9 @@ var jsApp	=
 		Initialize the jsApp
 		
 		---			*/
-	onload: function()
-	{
+	onload: function() {
 		
-		if (!me.video.init('jsapp', 800, 480))
-		{
+		if (!me.video.init('jsapp', 800, 480)) {
 			alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
 			return;
 		}
@@ -49,7 +41,6 @@ var jsApp	=
 		me.input.bindKey(me.input.KEY.RIGHT, "right");
 		me.input.bindKey(me.input.KEY.UP,	 "up");
 		me.input.bindKey(me.input.KEY.DOWN,	 "down");
-		me.input.bindKey(me.input.KEY.ENTER, "enter");
 
         // overrides the entity gravity values
         me.sys.gravity = 0;
@@ -72,41 +63,7 @@ var jsApp	=
 		 rendering loop
 		
 		---										*/
-	onUpdateFrame: function()
-	{
-	
-		// navigate the map :)
-		if (me.input.isKeyPressed('left'))
-		{
-			me.game.viewport.move(-(me.game.currentLevel.tilewidth/2),0);
-			// force redraw
-			me.game.repaint();
-			
-		}
-		else if (me.input.isKeyPressed('right'))
-      {
-			me.game.viewport.move(me.game.currentLevel.tilewidth/2,0);		
-			// force redraw
-			me.game.repaint();
-		}
-				
-		if (me.input.isKeyPressed('up'))
-		{
-			me.game.viewport.move(0,-(me.game.currentLevel.tileheight/2));
-			// force redraw
-			me.game.repaint();
-		}
-		else if (me.input.isKeyPressed('down'))
-      {
-			me.game.viewport.move(0,me.game.currentLevel.tileheight/2);
-			// force redraw
-			me.game.repaint();
-		}
-
-		if (me.input.isKeyPressed('enter')) {
-			me.game.viewport.shake(16, 500);
-		}
-
+	onUpdateFrame: function() {
 		// update the frame counter
 		me.timer.update();
 
@@ -123,47 +80,18 @@ var jsApp	=
 		 using the listbox current value in the HTML file
 		
 		---										*/
-	changelevel: function()
-	{
-		var level_id = document.getElementById("level_name").value;
-		switch (level_id)
-		{
-			case "1":
-				level = "village";
-				break;
-			case "2":
-				level = "desert";
-				break;
-			case "3":
-				level = "sewers";
-				break;
-			case "4":
-				level = "cute";
-				break;
-			case "5":
-				level = "isometric";
-				break;
-			case "6":
-				level = "perspective";
-				break;
-			default:
-				return;
-		};
-		
+	changelevel: function() {
 		// reset everything
 		me.game.reset();
 		
 		// load the new level
 		me.levelDirector.loadLevel(level);		
-
-
 	}
 
 }; // jsApp
 
 
 //bootstrap :)
-window.onReady(function() 
-{
+window.onReady(function() {
 	jsApp.onload();
 });
